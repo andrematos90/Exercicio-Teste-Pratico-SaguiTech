8710525841f023c526ac546b9911d22028aba4f0
André Matos <andrermatos90@gmail.com>
2023-12-01 15:59:26 -0300

    pagina inicial

--- Exercicio teste refeito em Angular/notafiscal/src/app/app.module.ts
+++ Exercicio teste refeito em Angular/notafiscal/src/app/app.module.ts
@@ -5,16 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/page/home/home.component';
 import { ResultComponent } from './components/page/result/result.component';
+import { CalculatorComponent } from './components/page/calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
-    ResultComponent
+    ResultComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
