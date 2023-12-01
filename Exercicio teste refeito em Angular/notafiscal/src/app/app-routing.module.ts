fc3ed894a5c32d3ddcbffb5e8c09d0356be63d0d
André Matos <andrermatos90@gmail.com>
2023-12-01 16:25:35 -0300

    ..

--- Exercicio teste refeito em Angular/notafiscal/src/app/app-routing.module.ts
+++ Exercicio teste refeito em Angular/notafiscal/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalculatorComponent } from './components/page/calculator/calculator.component';
+import { ResultComponent } from './components/page/result/result.component';
 
 const routes: Routes = [
-  {path: '', component: CalculatorComponent}
+  {path: '', component: CalculatorComponent},
+  {path: 'notafiscal', component: ResultComponent},
 
 ];
 
